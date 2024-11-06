02017ed116870c1f8be512404295fdb6fe7e011a
selorcav <se.lorcav@gmail.com>
2024-11-05 22:59:52 -0300

    testing navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // App.js
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import routes from './routes';
+import Nav from './components/Nav.component';
 
 function App() {
     return (
         <Router>
+            <Nav />
             {routes.map((route, index) => (
                 <Route key={index} path={route.path} component={route.component} />
             ))}
