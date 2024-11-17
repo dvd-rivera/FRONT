9b4705651f1d24e66bdd7cbdd4b687f4465568de
dvd-rivera <51865488+dvd-rivera@users.noreply.github.com>
2024-11-16 22:07:11 -0300

    correción doble link en el nav

--- src/components/nav-components/navCart.component.tsx
+++ src/components/nav-components/navCart.component.tsx
@@ -4,33 +4,24 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import { CartContext } from '../../context/cart.context'
 
 const NavCartComponent: React.FC = () => {
-  const cartContext = useContext(CartContext)
+    const cartContext = useContext(CartContext)
 
-  if (!cartContext) {
-    return null 
-  }
+    if (!cartContext) {
+        return null
+    }
 
-  const { productsInCart } = cartContext
+    const { productsInCart } = cartContext
 
-  const totalItems = productsInCart.reduce(
-    (total, product) => total + product.cant,
-    0
-  )
+    const totalItems = productsInCart.reduce((total, product) => total + product.cant, 0)
 
-  const qtyCart = "absolute bg-white rounded-xl py-1 px-2 text-xs top-0 right-0"
+    const qtyCart = 'absolute bg-white rounded-xl py-1 px-2 text-xs top-0 right-0'
 
-  return (
-    <Link to="/cart">
-      <div className="relative p-3">
-        <ShoppingCartIcon style={{ fontSize: 32, color: '#000000' }} />
-        {totalItems > 0 && (
-          <span className={qtyCart}>
-            {totalItems}
-          </span>
-        )}
-      </div>
-    </Link>
-  )
+    return (
+        <div className="relative p-3">
+            <ShoppingCartIcon style={{ fontSize: 32, color: '#000000' }} />
+            {totalItems > 0 && <span className={qtyCart}>{totalItems}</span>}
+        </div>
+    )
 }
 
 export default NavCartComponent
