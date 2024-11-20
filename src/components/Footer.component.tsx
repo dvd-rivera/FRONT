e96250de0e2e9d9148a28b94b157a80588bfa216
dvd-rivera <51865488+dvd-rivera@users.noreply.github.com>
2024-11-20 19:41:30 -0300

    se corrige error de src en nav y footer del logo

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -3,37 +3,37 @@ import InstagramIcon from '@mui/icons-material/Instagram'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 
 const FooterComponent: React.FC = () => {
-  return (
-      <footer>
-          <div className="container">
-              <div className="contact-section">
-                  <h3 className="footer-title">Contáctanos</h3>
-                  <p>¿Necesitas ponerte en contacto con nosotros?</p>
-                  <p className="mail">contacto@happyart.cl</p>
-                  <div className="social-container">
-                      <InstagramIcon style={{ fontSize: 40, color: '#FF8DC0' }}></InstagramIcon>
-                      <WhatsAppIcon style={{ fontSize: 40, color: '#FF8DC0' }}></WhatsAppIcon>
-                  </div>
-              </div>
-              <div className="contact-section">
-                  <h3 className="footer-title">Ayuda</h3>
-                  <p>Preguntas Frecuentes</p>
-                  <p>Información de Despachos</p>
-                  <p>Formas de Pago</p>
-                  <p>Cambios y Devoluciones</p>
-              </div>
-              <div className="contact-section">
-                  <h3 className="footer-title">Sobre Nosotros</h3>
-                  <p>Quienes Somos</p>
-                  <p>Regalos Corporativos</p>
-                  <p>Distribución y venta mayorista</p>
-              </div>
-              <div className="brand-container">
-                  <img src="img/happyArt-v.svg" alt="" />
-              </div>
-          </div>
-      </footer>
-  )
+    return (
+        <footer>
+            <div className="container">
+                <div className="contact-section">
+                    <h3 className="footer-title">Contáctanos</h3>
+                    <p>¿Necesitas ponerte en contacto con nosotros?</p>
+                    <p className="mail">contacto@happyart.cl</p>
+                    <div className="social-container">
+                        <InstagramIcon style={{ fontSize: 40, color: '#FF8DC0' }}></InstagramIcon>
+                        <WhatsAppIcon style={{ fontSize: 40, color: '#FF8DC0' }}></WhatsAppIcon>
+                    </div>
+                </div>
+                <div className="contact-section">
+                    <h3 className="footer-title">Ayuda</h3>
+                    <p>Preguntas Frecuentes</p>
+                    <p>Información de Despachos</p>
+                    <p>Formas de Pago</p>
+                    <p>Cambios y Devoluciones</p>
+                </div>
+                <div className="contact-section">
+                    <h3 className="footer-title">Sobre Nosotros</h3>
+                    <p>Quienes Somos</p>
+                    <p>Regalos Corporativos</p>
+                    <p>Distribución y venta mayorista</p>
+                </div>
+                <div className="brand-container">
+                    <img src="/img/happyArt-v.svg" alt="" />
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default FooterComponent
--- src/components/Nav.component.tsx
+++ src/components/Nav.component.tsx
@@ -1,88 +1,93 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import SearchComponent from './nav-components/search.component';
-import NavCartComponent from './nav-components/navCart.component';
-import CategoriesMenu from './nav-components/categoriesMenu.component';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState, useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import SearchComponent from './nav-components/search.component'
+import NavCartComponent from './nav-components/navCart.component'
+import CategoriesMenu from './nav-components/categoriesMenu.component'
+import MenuIcon from '@mui/icons-material/Menu'
+import UserMenu from './nav-components/userMenu.component'
 
 const NavComponent: React.FC = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
+    const [menuOpen, setMenuOpen] = useState(false)
+    const menuRef = useRef<HTMLDivElement>(null)
 
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-      setMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    if (menuOpen) {
-      document.body.classList.add('overflow-hidden');
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.body.classList.remove('overflow-hidden');
-      document.removeEventListener('mousedown', handleClickOutside);
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen)
     }
 
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [menuOpen]);
+    const handleClickOutside = (event: MouseEvent) => {
+        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setMenuOpen(false)
+        }
+    }
 
-  return (
-    <nav className="bg-white fixed top-0 left-0 w-full z-10">
-      <div className="flex lg:w-11/12 xl:w-10/12 mx-auto py-2 px-4">
-        <div className="lg:w-2/12 w-full justify-between flex">
-          <Link to="/">
-            <img src="img/happyArt-h.svg" alt="Logo Happy Art" />
-          </Link>
-          <div className="block lg:hidden text-3xl">
-            <button onClick={handleMenuToggle}>
-              <MenuIcon />
-            </button>
-          </div>
-        </div>
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.classList.add('overflow-hidden')
+            document.addEventListener('mousedown', handleClickOutside)
+        } else {
+            document.body.classList.remove('overflow-hidden')
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
 
-        <div
-          ref={menuRef}
-          className={`flex lg:w-10/12  items-center justify-start lg:justify-between bg-white lg:h-auto h-screen absolute lg:relative top-0 ${
-            menuOpen ? 'left-0' : '-left-full'
-          } flex-col lg:flex-row py-4 lg:py-0 transition-all duration-500 lg:left-0`}
-        >
-          <div className="lg:w-3/12">
-            <div className="flex justify-center lg:hidden">
-              <Link to="/">
-                <img className='my-4' src="img/happyArt-h.svg" alt="Logo Happy Art" />
-              </Link>
-            </div>
-            <div className="Categories-container lg:block">
-              <CategoriesMenu />
-            </div>
-          </div>
-          <div className="flex lg:w-9/12 items-center justify-end flex-col lg:flex-row">
-            <div className="search-container mx-4 my-3 lg:my-0 order-2 lg:order-last">
-              <SearchComponent />
-            </div>
-            <div className="personalized-link-container mx-4 transition hover:bg-slate-100 px-4 rounded-md my-1 lg:my-0 lg:w-auto w-full lg:text-left text-center py-3 order-3 lg:order-last">
-              <Link to="/personalizados">Personalizados</Link>
-            </div>
-            <div className="login-container mx-4 transition hover:bg-slate-100 px-4 rounded-md my-1 lg:my-0 lg:w-auto w-full lg:text-left text-center py-3 order-2 lg:order-last">
-              <Link to="/login">Inicia sesión</Link>
-            </div>
-            <div className="cart-container my-3 lg:my-0 order-1 lg:order-last">
-              <Link to="/cart">
-                <NavCartComponent />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [menuOpen])
 
-export default NavComponent;
+    return (
+        <nav className="bg-white fixed top-0 left-0 w-full z-10">
+            <div className="flex lg:w-11/12 xl:w-10/12 mx-auto py-2 px-4">
+                <div className="lg:w-2/12 w-full justify-between flex">
+                    <Link to="/" className="flex justify-center align-center">
+                        <img src="/img/happyArt-h.svg" alt="Logo Happy Art" />
+                    </Link>
+                    <div className="block lg:hidden text-3xl">
+                        <button onClick={handleMenuToggle}>
+                            <MenuIcon />
+                        </button>
+                    </div>
+                </div>
+
+                <div
+                    ref={menuRef}
+                    className={`flex lg:w-10/12 items-center justify-start lg:justify-between bg-white lg:h-auto h-screen absolute lg:relative top-0 ${
+                        menuOpen ? 'left-0' : '-left-full'
+                    } flex-col lg:flex-row py-4 lg:py-0 transition-all duration-500 lg:left-0`}
+                >
+                    <div className="lg:w-3/12">
+                        <div className="flex justify-center lg:hidden">
+                            <Link to="/">
+                                <img
+                                    className="my-4"
+                                    src="/img/happyArt-h.svg"
+                                    alt="Logo Happy Art"
+                                />
+                            </Link>
+                        </div>
+                        <div className="Categories-container lg:block">
+                            <CategoriesMenu />
+                        </div>
+                    </div>
+                    <div className="search-container mx-4 my-3 lg:my-0">
+                        <SearchComponent />
+                    </div>
+
+                    {/* Link de personalizados */}
+                    <div className="personalized-link-container mx-4 transition hover:bg-slate-100 px-4 rounded-md my-1 lg:my-0 lg:w-auto w-full lg:text-left text-center py-3">
+                        <Link to="/personalizados">Personalizados</Link>
+                    </div>
+                    <div className="flex lg:w-9/12 items-center justify-end flex-col lg:flex-row">
+                        <UserMenu></UserMenu>
+                        <div className="cart-container my-3 lg:my-0 order-1 lg:order-last">
+                            <Link to="/cart">
+                                <NavCartComponent />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavComponent
